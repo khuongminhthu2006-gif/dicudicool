11a98165d8639eae0542ad3559d8ae1c86989ac6
khuongminhthu2006-gif <khuongminhthu2006@gmail.com>
2026-05-15 19:06:37 +0700

    fix route

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ const startServer = async () => {
   if (!isCombinedDevServer) {
     app.use(express.static(path.join(__dirname, 'dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     });
   }
